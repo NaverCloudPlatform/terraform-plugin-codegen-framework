c2cc4bbb71201dd3e9a9d3ff9341a0600fed3fd9
Geun-Oh <kandy1002@naver.com>
2024-12-04 17:19:29 +0900

    feat: Add ImportState rendering logic

--- internal/output/write.go
+++ internal/output/write.go
@@ -125,6 +125,11 @@ func WriteResources(resourcesSchema, resourcesModels, customTypeValue, resources
 			return err
 		}
 
+		_, err = f.Write(n.RenderImportState())
+		if err != nil {
+			return err
+		}
+
 		_, err = f.Write(n.RenderCreate())
 		if err != nil {
 			return err
@@ -156,6 +161,95 @@ func WriteResources(resourcesSchema, resourcesModels, customTypeValue, resources
 		}
 
 		_, err = f.Write(n.RenderWait())
+		if err != nil {
+			return err
+		}
+
+		filePath := f.Name()
+
+		util.RemoveDuplicates(filePath)
+	}
+
+	return nil
+}
+
+// WriteResources uses the packageName to determine whether to create a directory and package per resource.
+// If packageName is an empty string, this indicates that the flag was not set, and the default behaviour is
+// then to create a package and directory per resource. If packageName is set then all generated code is
+// placed into the same directory and package.
+// CORE - 여기에 줄을 추가하여 생성하는 것으로 한다.
+func WriteResourceTests(resourcesSchema, resourcesModels, customTypeValue, resourcesToFrom map[string][]byte, outputDir, packageName string) error {
+	for k := range resourcesSchema {
+		dirName := ""
+
+		if packageName == "" {
+			dirName = fmt.Sprintf("resource_%s", k)
+
+			err := os.MkdirAll(filepath.Join(outputDir, dirName), os.ModePerm)
+			if err != nil {
+				return err
+			}
+		}
+
+		filename := fmt.Sprintf("%s_resource_gen_test.go", k)
+
+		configPath := util.MustAbs("./internal/generator_config_apigw.yml")
+		codeSpecPath := util.MustAbs("./internal/example-code-spec.json")
+
+		// 추후 다중 자원 생성을 진행할 때 이곳에서 반복문을 수행해야하므로 resourceName을 이곳에서 선언한다.
+
+		n := ncloud.New(configPath, codeSpecPath, k)
+
+		f, err := os.Create(filepath.Join(outputDir, dirName, filename))
+		if err != nil {
+			return err
+		}
+
+		// CORE - 이곳에 코드를 추가한다.
+		_, err = f.Write(n.RenderTest())
+		if err != nil {
+			return err
+		}
+
+		filePath := f.Name()
+
+		util.RemoveDuplicates(filePath)
+	}
+
+	return nil
+}
+
+// WriteDataSources uses the packageName to determine whether to create a directory and package per data source.
+// If packageName is an empty string, this indicates that the flag was not set, and the default behaviour is
+// then to create a package and directory per data source. If packageName is set then all generated code is
+// placed into the same directory and package.
+func WriteDataSourceTests(dataSourcesSchema, dataSourcesModels, customTypeValue, dataSourcesToFrom map[string][]byte, outputDir, packageName string) error {
+	for k := range dataSourcesSchema {
+		dirName := ""
+
+		if packageName == "" {
+			dirName = fmt.Sprintf("datasource_%s", k)
+
+			err := os.MkdirAll(filepath.Join(outputDir, dirName), os.ModePerm)
+			if err != nil {
+				return err
+			}
+		}
+
+		filename := fmt.Sprintf("%s_data_source_gen_test.go", k)
+
+		configPath := util.MustAbs("./internal/generator_config_apigw.yml")
+		codeSpecPath := util.MustAbs("./internal/example-code-spec.json")
+
+		n := ncloud.New(configPath, codeSpecPath, k)
+
+		f, err := os.Create(filepath.Join(outputDir, dirName, filename))
+		if err != nil {
+			return err
+		}
+
+		// CORE - 이곳에 코드를 추가한다.
+		_, err = f.Write(n.RenderTest())
 		if err != nil {
 			return err
 		}
