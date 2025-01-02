720d48809384bf855d6bc56f7de445fa5d59563e
Geun-Oh <kandy1002@naver.com>
2025-01-02 10:14:14 +0900

    fix: foramt optional keys to PascalCase

--- internal/ncloud/template.go
+++ internal/ncloud/template.go
@@ -446,7 +446,7 @@ func New(spec util.NcloudSpecification, resourceName, packageName string) *Templ
 		createOpOptionalParam = createOpOptionalParam + fmt.Sprintf(`
 		if !plan.%[1]s.IsNull() && !plan.%[1]s.IsUnknown() {
 			reqParams.%[1]s = plan.%[1]s.ValueString()
-		},`, util.ToPascalCase(val)) + "\n"
+		}`, util.FirstAlphabetToUpperCase(val)) + "\n"
 	}
 
 	for _, val := range targetResourceRequest.Update[0].RequestBody.Optional {
@@ -457,7 +457,7 @@ func New(spec util.NcloudSpecification, resourceName, packageName string) *Templ
 		updateOpOptionalParam = updateOpOptionalParam + fmt.Sprintf(`
 		if !plan.%[1]s.IsNull() && !plan.%[1]s.IsUnknown() {
 			reqParams.%[1]s = plan.%[1]s.ValueString()
-		},`, util.ToPascalCase(val)) + "\n"
+		}`, util.FirstAlphabetToUpperCase(val)) + "\n"
 	}
 
 	t.funcMap = funcMap
