bd9474b6e479d55981177c4ace2281b5f150be66
Geun-Oh <kandy1002@naver.com>
2025-01-20 10:14:47 +0900

    fix: rename files to distingush with data source

--- internal/ncloud/embed.go
+++ internal/ncloud/embed.go
@@ -4,13 +4,13 @@ import (
 	_ "embed"
 )
 
-//go:embed templates/initial.go.tpl
+//go:embed templates/initial_resource.go.tpl
 var InitialTemplate string
 
 //go:embed templates/create.go.tpl
 var CreateTemplate string
 
-//go:embed templates/read.go.tpl
+//go:embed templates/read_resource.go.tpl
 var ReadTemplate string
 
 //go:embed templates/update.go.tpl
@@ -19,16 +19,16 @@ var UpdateTemplate string
 //go:embed templates/delete.go.tpl
 var DeleteTemplate string
 
-//go:embed templates/model.go.tpl
+//go:embed templates/model_resource.go.tpl
 var ModelTemplate string
 
-//go:embed templates/refresh.go.tpl
+//go:embed templates/refresh_resource.go.tpl
 var RefreshTemplate string
 
 //go:embed templates/wait.go.tpl
 var WaitTemplate string
 
-//go:embed templates/test.go.tpl
+//go:embed templates/test_resource.go.tpl
 var TestTemplate string
 
 //go:embed templates/import.go.tpl
